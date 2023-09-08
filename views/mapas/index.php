f82754afa0f2c726ecd4ef317adb7d5c6369af99
dannycornelio <licdannycornelio@gmail.com>
2023-09-08 16:05:28 -0600

    integracion de un boton para actualizar el mapa

--- views/mapas/index.php
+++ views/mapas/index.php
@@ -1,6 +1,11 @@
 <h1 class="text-center">PRUEBA DE MAPAS</h1>
-<div class="row justify-content-center mb-3">
-    <div class="col-lg-11 p-3 border rounded" id="map" style="height: 60vh;min-height:auto">
+<div class="text-center">
+    <div class="btn-group" role="group">
+        <button class="btn btn-danger btn-sm" id="actualizar" name="actualizar">ACTUALIZAR</button>
+        <a class="btn btn-primary btn-sm" href="/mapa_leaflet/mapas"><i class="bi bi-globe-americas me-2"></i> REINICIAR</a>
     </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-lg-11 border rounded" id="map" style="height: 60vh; min-height:auto "></div>
 </div>
 <script src="<?= asset('./build/js/mapas/index.js')?>"></script>
